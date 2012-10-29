391bfbf712a7e8e82b579bdd509d4267d5f7aab0
Ladutenko Konstantin <kostya at ladutenko dot com>
2012-10-29 16:01:14 +0400

    Add of inintal support of test mode to go.sh

--- go.sh
+++ go.sh
@@ -50,7 +50,7 @@ Onza_MPI_size   \t- total number of MPI processes
 Onza_MPI_nodes  \t- total number of MPI nodes for cluster enviroment
 \n
 In the case of enviroment parameters are not (or set with value 'unset')\n
-set some default vale (depending on executin host) will be used.\n"
+set some default vale (depending on executing host) will be used.\n"
 #############################################################################
 if [[ ! $Onza_MPI_size ]]; then Onza_MPI_size="unset"; fi
 if [[ ! $Onza_MPI_nodes ]]; then Onza_MPI_nodes="unset"; fi
@@ -79,6 +79,9 @@ usedCompiler=$compiler_gcc # or clang
 yes="yes";        no="no"
 isNew=$yes;       isTest=$no ;               isProfile=$no
 path_onza=$PWD;   path_bin=$path_onza/bin;   path_build=$path_onza/build
+path_src=$path_onza/src
+# Should be same as in cmake config in $path_src
+onza_bin="run-onza-fdtd"
 if [[ ! $mode ]]; then  mode="new"; fi
 if [[ $mode = "help" || $mode = "--help" || $mode = "-h" ]]; then
  echo -e $USAGE
@@ -110,23 +113,31 @@ if [[ $mode = $mode_test && $configFile ]]; then
     exit 1
 fi
 # Check config file(s)
-path_testX1Dzero=$path_onza/data/testX1Dzero.config
-path_testTMz2Dspeedup=$path_onza/data/testTMz2Dspeedup.config
-path_test3Dsimple=$path_onza/data/test3Dsimple.config
+tests="testX1Dzero testTMz2Dspeedup test3Dsimple"
+for test in $tests; do
+    path_test_config="path_${test}_config"
+    eval path_${test}_config=$path_onza/data/$test.config  
+    echo ${!path_test_config}
+done
+# path_testX1Dzero_config=$path_onza/data/testX1Dzero.config
+# path_testTMz2Dspeedup_config=$path_onza/data/testTMz2Dspeedup.config
+# path_test3Dsimple_config=$path_onza/data/test3Dsimple.config
 path_default_config=$path_onza/data/default-onza.config
 if [[ $mode = $mode_test ]]; then
     echo Check for tests config files...
-    if [[ ! -r $path_testX1Dzero || ! -r $path_testTMz2Dspeedup || \
-        ! -r $path_test3Dsimple ]];
-    then
-        echo ================ !ERROR! =================
-        echo Test mode was not able to access some of config files for tests
-        echo $path_testX1Dzero
-        echo $path_testTMz2Dspeedup
-        echo $path_test3Dsimple
-        echo ================ !ERROR! =================
-        exit 1
-    fi
+    for test in $tests; do
+        path_test_config="path_${test}_config"
+        eval path_${test}_config=$path_onza/data/$test.config  
+        echo ${!path_test_config}
+        if [[ ! -r ${!path_test_config} ]];
+        then
+            echo ================ !ERROR! =================
+            echo Test mode was not able to access some of config files for tests
+            echo ${!path_test_config}
+            echo ================ !ERROR! =================
+            exit 1
+        fi
+    done
 fi
 # Should be tested after checking $test_mode 
 if [ ! $configFile ]; then
@@ -150,18 +161,19 @@ echo mode: $mode
 echo config file: $configFile
 echo base dir path: $path_onza
 # Check directory structure
-path_src=$path_onza/src
 if [[ ! -d $path_src ]]; then
     echo ================ !ERROR! =================
     echo No source folder $path_src
     echo ================ !ERROR! =================
     exit
 fi
+force_new_build=$no
 if [[ -a $path_build ]]; then
     echo Found build folder.
 else
     echo Creating build folder...
     mkdir build
+    force_new_build=$yes
 fi
 if [[ -a $path_bin ]]; then
     echo Found bin folder.
@@ -173,7 +185,8 @@ fi
 #   Compile settings
 #############################################################################
 echo ============ Compile settings =============
-if [[ $mode = $mode_old || $mode = $mode_old2 || $mode = $mode_old2prof ]]; then
+if [[ ( $mode = $mode_old || $mode = $mode_old2 || $mode = $mode_old2prof || \
+    $mode = $mode_test ) && $force_new_build = $no ]]; then
     isNew=$no
     echo Recompile mode is on.
 else
@@ -227,6 +240,7 @@ make install
 #   Run
 #############################################################################
 HOST=`cat /etc/hostname`
+echo "Executing on host -> $HOST <-"
 if [[ $mode = $mode_test ]]; then isTest=$yes; fi
 if [[ $isProfile = $yes ]]; then 
     # Grpof output for each process in separate file
@@ -242,41 +256,79 @@ if [[ $isTest = $no ]]; then
         if [[ $Onza_MPI_nodes = "unset" ]]; then Onza_MPI_nodes=8; fi
         echo "(1) Nodes $Onza_MPI_nodes procs $Onza_MPI_size"
         salloc -N $Onza_MPI_nodes -n $Onza_MPI_size -p max1hour \
-            mpirun $MPIoptions ./run-onza-fdtd onza.config
+            mpirun $MPIoptions ./$onza_bin onza.config
     elif  [[ $HOST == "deb00" || $HOST == "dmmrkovich-birzha" ]]; then
         if [[ $Onza_MPI_size = "unset" ]]; then Onza_MPI_size=4; fi
         echo "(1) Nodes 1  procs $Onza_MPI_size"
-        mpirun -np $Onza_MPI_size $MPIoptions ./run-onza-fdtd onza.config
+        mpirun -np $Onza_MPI_size $MPIoptions ./$onza_bin onza.config
     else
         if [[ $Onza_MPI_size = "unset" ]]; then Onza_MPI_size=2; fi
         echo "(1) Nodes 1  procs $Onza_MPI_size"
-        mpirun -np $Onza_MPI_size $MPIoptions ./run-onza-fdtd onza.config
+        mpirun -np $Onza_MPI_size $MPIoptions ./$onza_bin onza.config
     fi
 fi  # end of if [[ $isTest = $no ]]
-
 if [[ $isProfile = $yes ]]; then 
-    gprof  run-onza-fdtd gmon.out* | grep '^index'
+    gprof  $onza_bin gmon.out* | grep '^index'
     for file in gmon.out*;
     do
         echo $file
-        gprof  run-onza-fdtd $file | grep 'DoBorderStep' | grep '^\['
-        gprof  run-onza-fdtd $file | grep 'DoStep' | grep '^\['
+        gprof  $onza_bin $file | grep 'DoBorderStep' | grep '^\['
+        gprof  $onza_bin $file | grep 'DoStep' | grep '^\['
     done
     echo Average
-    gprof  run-onza-fdtd gmon.out* | grep 'DoBorderStep' | grep '^\['
-    gprof  run-onza-fdtd gmon.out* | grep 'DoStep' | grep '^\['
-    gprof --no-flat-profile run-onza-fdtd gmon.out* > average-profile
-    gprof run-onza-fdtd gmon.out* > flat-average-profile
+    gprof  $onza_bin gmon.out* | grep 'DoBorderStep' | grep '^\['
+    gprof  $onza_bin gmon.out* | grep 'DoStep' | grep '^\['
+    gprof --no-flat-profile $onza_bin gmon.out* > average-profile
+    gprof $onza_bin gmon.out* > flat-average-profile
     rm gmon.out*
 fi  # end of if isProfile
 if [[ $isTest = $yes ]]; then
-    echo TEST
-fi
-if [[ $mode != $mode_test && $configFile = $path_testX1Dzero ]]; then
-    echo "Prepare *.png from gnuplot ..."
+    echo "Prepare files for tests ..."
     cd $path_bin
-    cp $path_onza/data/gnuplot/* ./
-    ./gnuplot-all.sh >/dev/null  2>&1
-fi
+    for test in $tests; do
+        # For each test make dir, copy config and binary to it, run.
+        cd $path_bin
+        mkdir $test
+        path_test="$path_bin/$test"  
+        path_test_config="path_${test}_config"
+        cp ${!path_test_config} ${path_test}/onza.config
+        cp $onza_bin $path_test
+        cd $path_test
+        backup_Onza_MPI_size=$Onza_MPI_size
+        backup_Onza_MPI_nodes=$Onza_MPI_nodes
+        if [[ $test = "testX1Dzero" ]]; then
+            Onza_MPI_size=1
+            Onza_MPI_nodes=1
+        fi
+        echo "============ Running test $test ============="
+        if [[ $HOST == "head.phoif.ifmo.ru" ]]; then
+            echo "Waiting for shared file system to distibute files..."
+            sleep 2
+            if [[ $Onza_MPI_size = "unset" ]]; then Onza_MPI_size=16; fi
+            if [[ $Onza_MPI_nodes = "unset" ]]; then Onza_MPI_nodes=8; fi
+            echo "(1) Nodes $Onza_MPI_nodes procs $Onza_MPI_size"
+            salloc -N $Onza_MPI_nodes -n $Onza_MPI_size -p max1hour \
+                mpirun $MPIoptions ./$onza_bin onza.config
+        elif  [[ $HOST == "deb00" || $HOST == "dmmrkovich-birzha" ]]; then
+            if [[ $Onza_MPI_size = "unset" ]]; then Onza_MPI_size=4; fi
+            echo "(1) Nodes 1  procs $Onza_MPI_size"
+            mpirun -np $Onza_MPI_size $MPIoptions ./$onza_bin onza.config
+        else
+            if [[ $Onza_MPI_size = "unset" ]]; then Onza_MPI_size=2; fi
+            echo "(1) Nodes 1  procs $Onza_MPI_size"
+            mpirun -np $Onza_MPI_size $MPIoptions ./$onza_bin onza.config
+        fi
+        if [[ $test = "testX1Dzero" ]]; then
+            echo "Prepare *.png from gnuplot ..."
+            cp $path_onza/data/gnuplot/* ./
+            ./gnuplot-all.sh >/dev/null  2>&1
+            cp *02??-*.png $path_bin/
+            rm *.png
+        fi
+        Onza_MPI_size=$backup_Onza_MPI_size
+        Onza_MPI_nodes=$backup_Onza_MPI_nodes
+        rm *.onza  >/dev/null  2>&1
+    done  # end of for test in $tests; do
+fi  # end of if [[ $isTest = $yes ]]
 rm *.onza  >/dev/null  2>&1
     
